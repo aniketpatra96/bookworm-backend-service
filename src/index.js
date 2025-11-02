81286b861af6cca3660c480a4e0297c5d59ec746
ANIKET PATRA <110764399+aniketpatra96@users.noreply.github.com>
2025-11-02 10:26:25 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import authRouter from './routes/authRoutes.route.js'
 import connectDB from './lib/db.js'
 import cors from 'cors'
 import bookRouter from './routes/bookRoutes.route.js'
+import job from "./lib/cron.js";
 
 const app = express()
 const PORT = process.env.PORT
 
+job.start();
 app.use(express.json())
 app.use(cors())
 app.use('/api/auth',authRouter)
---
+++ src/lib/cron.js
@@ -0,0 +1,18 @@
+import { error, log } from "console";
+import cron from "cron";
+import https from "https";
+
+const job = new cron.CronJob("*/* 14 * * * *", function () {
+  console.log("You will see this message every 14 minutes");
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200)
+        console.log("Pinged successfully to prevent idling.");
+      else console.log("Failed to ping the server.", res.statusCode);
+    })
+    .on("error", (e) => {
+      console.error("Error while pinging the server: ", e);
+    });
+});
+
+export default job;
