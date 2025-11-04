2278cea92da6e6d0045e366c279b61a639ce726d
ANIKET PATRA <110764399+aniketpatra96@users.noreply.github.com>
2025-11-04 09:18:30 +0530

    increased limit for base64 image

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import express from 'express'
-import 'dotenv/config'
-import authRouter from './routes/authRoutes.route.js'
-import connectDB from './lib/db.js'
-import cors from 'cors'
-import bookRouter from './routes/bookRoutes.route.js'
+import express from "express";
+import "dotenv/config";
+import authRouter from "./routes/authRoutes.route.js";
+import connectDB from "./lib/db.js";
+import cors from "cors";
+import bookRouter from "./routes/bookRoutes.route.js";
 import job from "./lib/cron.js";
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT;
 
 job.start();
 app.use(cors());
-app.use(express.json());
-app.use('/api/auth',authRouter)
-app.use('/api/books', bookRouter)
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use("/api/auth", authRouter);
+app.use("/api/books", bookRouter);
 
-app.get('/', async (req,res) => {
-    res.send('BOOKWORM SERVER IS RUNNING!!')
-})
+app.get("/", async (req, res) => {
+  res.send("BOOKWORM SERVER IS RUNNING!!");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}!`)
-    connectDB()
-})
+  console.log(`Server is running on port ${PORT}!`);
+  connectDB();
+});
